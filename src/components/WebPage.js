3836354f45ff6b9aee8dc8bf30d55d152ca7d5a5
simongfxu <xugaofan@gmail.com>
2019-02-13 14:19:59 +0800

    webpage 优化

--- src/components/WebPage.js
+++ src/components/WebPage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { Text, View, Platform, Alert, ActivityIndicator, StyleSheet } from 'react-native'
+import { Text, View, Platform, Alert, ActivityIndicator, StyleSheet, Linking } from 'react-native'
 import { WebView } from 'react-native-webview'
 import PropTypes from 'prop-types'
 
-const UA = `CoinCola/1.0.0 ${Platform.OS === 'ios' ? 'iPhone' : 'Android'} Mobile`
+const UA = `CC MJB/1.0.0 ${Platform.OS === 'ios' ? 'iPhone' : 'Android'} Mobile`
 const styles = StyleSheet.create({
   webview: {
     justifyContent: 'center',
@@ -28,14 +28,24 @@ class WebPage extends Component {
     source: PropTypes.object.isRequired
   }
 
+  state = {
+    message: ''
+  }
+
   onError = e => {
+    this.setState({
+      message: e.message
+    })
     Alert.alert('网页加载出错')
   }
 
   renderError = e => {
     return (
       <View style={styles.webview}>
-        <Text style={styles.activityIndicator}>网页加载失败</Text>
+        <Text style={styles.activityIndicator}>
+          网页加载失败
+          { this.state.message }
+        </Text>
       </View>
     )
   }
@@ -50,6 +60,23 @@ class WebPage extends Component {
     )
   }
 
+  onShouldStartLoadWithRequest = e => {
+    const { url = '' } = e
+    if (url.startsWith('http:') || url.startsWith('https:')) {
+      return true
+    }
+
+    Linking.canOpenURL(url).then(result => {
+      if (result) {
+        Linking.openURL(url)
+      } else {
+        console.log(`could not open url ${url}`)
+      }
+    })
+
+    return false
+  }
+
   render () {
     const { source, ...rest } = this.props
 
@@ -62,6 +89,7 @@ class WebPage extends Component {
         onError={this.onError}
         renderLoading={this.renderLoading}
         startInLoadingState
+        onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
         {...rest}
       />
     )
